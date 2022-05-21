2bcc3c1af0da81b103952dc4f11a27f5797c3fda
LesnikovYV <Y-levl@ya.ru>
2022-05-21 23:03:49 +0500

    Vfcbd 2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Эмассив 2");
+                    int arr [] = generateRandomArray();
+            //1 задание
+            int sum=0;
+            for (int i = 0; i< arr.length;i++){
+                sum= sum+arr[i];
+            }
+            System.out.println("Сумма трат за месяц составит " + sum + " рублей");
+
+            // 2 задание
+            int max = 99_999;
+            int min = 200_001;
+            for (int i = 0;i<arr.length; i++){
+                if (arr[i] < min){
+                    min =arr[i];
+                }
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+            }
+            System.out.printf("Минемальная сумма трат за день будет %d рублей. Максимальная сумма трат за день будет %d рублей. ", min, max);
+
+
+
+        }
+        private static int[] generateRandomArray() {
+            java.util.Random random = new java.util.Random();
+            int [] arr = new int[30];
+            for (int i=0; i<arr.length;i++){
+                arr[i]=random.nextInt( 100_000)+100_000;
+            }
+            return arr;
     }
 }
